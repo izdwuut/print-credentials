a85188767bbcb531bca3ab8726f69b2008a12c8b
Bartosz "izdwuut" Konikiewicz <bartosz.konikiewicz@gmail.com>
2018-01-21 13:24:04 +0100

    Add config

--- generate.sh
+++ generate.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-nol=$(cat credentials | wc -l)
-counter=$(cat counter)
-rm -f result.pwd
-if [ $(($counter + 2 + $nol)) -ge 59 ] #wordpad 11pt Courier New holds 59 lines
+config='config.cfg'
+nol=$(cat credentials.txt | wc -l)
+source $config
+function updateConfig {
+	sed -i "s/\($1 *= *\).*/\1$2/" $config
+}
+updateConfig 'prevCounter' $counter
+((spacing++))
+if [ $(($counter + $spacing + $nol)) -ge $maxNol ]
 then
 	counter=0
 	echo 'Remember to flip sides!'
+else
+	counter=$(($counter + $spacing))
 fi
-
-if [ $counter -gt 0 ]
-then
-	counter=$(($counter + 2))
-fi
-
 for (( i=0; i<$counter; i++ ))
 do
-	echo '' >> result.pwd
+	echo '' >> $result
 done
-
 counter=$(($counter + $nol))
-
-echo $counter > counter
-cat credentials >> result.pwd
+updateConfig 'counter' $counter
+cat $credentials >> $result
